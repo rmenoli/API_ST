c52a417d3ecb60b3aa2cca572ed78018a6b3ec8b
Riccardo Menoli Adecco <riccardo.menoli@adeccogroup.com>
2022-12-24 18:57:30 +0100

    Added possibility to add multiple entities in one file

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from sentence_transformers import SentenceTransformer
 from fastapi.responses import JSONResponse
-from fastapi import status, Depends
+from fastapi import status, Depends, File
 
 from db import get_list_all_keys, add_element_to_key, remove_element, knn_search, remove_all_elements, generate_index
 from exception import ValueAlreadyInDB, ValueNotInDB
@@ -16,7 +16,6 @@ app.include_router(auth_endpoints.router)
 
 st = SentenceTransformer(
     '/Users/riccardomenoli/Documents/ontology_nnew/ontology-service/models/normalization-models/sentece-trasformers/cross-en-de-roberta-sentence-transformer')
-
 
 
 # exception handler
@@ -49,9 +48,10 @@ def get_all_embedded_values():
 @app.get('/getClosestEntity/{entity_to_embed}',
          tags=['Embeddings exploration'],
          description='Get the closest entity in the DB closer to the inputted entity')
-def get_closest_entity(entity_to_embed):  # get method takes parameter form the link (2 lines above)
+def get_closest_entity(entity_to_embed: str, # get method takes parameter form the link (2 lines above)
+                       n_closes_element: int = 1):  # additional params specified after the path
     embeddings = utils.embed_entity(st, entity_to_embed)
-    return knn_search(embeddings, k=2)
+    return knn_search(embeddings, k=n_closes_element)
 
 
 class PostBody(BaseModel):
@@ -59,10 +59,10 @@ class PostBody(BaseModel):
 
 
 @app.post('/addElement',
-          tags=['Embeddings modification'],
+          tags=['Single embeddings modification'],
           description='Adds an element to the Db')
 def add_element_embedding_space(body_request: PostBody,
-                                token:str = Depends(o_auth)):
+                                token: str = Depends(o_auth)):
     # post method takes parms from the body, defined above the elemnts of the Body
     # token for authentication
     embeddings = utils.embed_entity(st, body_request.entity)
@@ -71,18 +71,31 @@ def add_element_embedding_space(body_request: PostBody,
 
 
 @app.post('/removeElement/{elem}',
-          tags=['Embeddings modification'],
+          tags=['Single embeddings modification'],
           description='Removes an element already present in the DB')
 def remove_element_embedding_space(body_request: PostBody,
-                                   token:str = Depends(o_auth)):
+                                   token: str = Depends(o_auth)):
     remove_element(body_request.entity)
     return 'element removed'
 
 
 @app.post('/removeAllElements',
-          tags=['Embeddings modification'],
+          tags=['Multiple embeddings modification'],
           description='Removes all element already present in the DB')
-def remove_all_elements_from_db(token:str = Depends(o_auth)):
+def remove_all_elements_from_db(token: str = Depends(o_auth)):
     remove_all_elements()
     generate_index()
     return 'All elements removed'
+
+
+@app.post('/AddMultipleElements',
+          tags=['Multiple embeddings modification'],
+          description='Add all the elements in the file .txt to the DB. If a element already exist it skip it')
+def add_elements_from_file(file: bytes = File(media_type='txt'), token: str = Depends(o_auth)):
+    file_content = file.decode('utf-8')
+    lines = file_content.split('\n')
+    for sentence in lines:
+        embeddings = utils.embed_entity(st, sentence)
+        add_element_to_key(sentence, embeddings)
+    out_string = f'Added elements: {file_content}'
+    return out_string
